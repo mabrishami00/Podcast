e4912d442c37e3128d499221e811def509f8274f
mabrishami00 <mabrishami78@yahoo.com>
2023-09-15 00:02:03 +0330

    feat: add bookmark model

--- interactions/models.py
+++ interactions/models.py
@@ -27,3 +27,9 @@ class Subscribe(models.Model):
     is_subscribed = models.BooleanField(default=True)
 
 
+class Bookmark(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
