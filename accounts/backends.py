91d1a1379c9b867e9f626731f7e2ed6704bf23d2
mabrishami00 <mabrishami78@yahoo.com>
2023-09-14 20:16:29 +0330

    feat: add generation of access token method

--- accounts/backends.py
+++ accounts/backends.py
@@ -40,3 +40,15 @@ class JWTAuthentication(authentication.BaseAuthentication):
             raise AuthenticationFailed("User not found")
 
         return user, payload
+    @classmethod
+    def generate_access_token(cls, user):
+        access_token_payload = {
+            "user_id": user.id,
+            "exp": datetime.datetime.now() + datetime.timedelta(days=0, minutes=5),
+            "iat": datetime.datetime.now(),
+            "jti": cls.jti,
+        }
+        access_token = jwt.encode(
+            access_token_payload, settings.SECRET_KEY, algorithm="HS256"
+        )
+        return access_token
