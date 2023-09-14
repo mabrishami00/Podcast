0fb9b157e4976a4f649d8a9b3292e959aecf79c3
mabrishami00 <mabrishami78@yahoo.com>
2023-09-14 20:16:03 +0330

    feat: add authenticate method

---
+++ accounts/backends.py
@@ -0,0 +1,42 @@
+from rest_framework import authentication
+import jwt
+from django.conf import settings
+import datetime
+import uuid
+from django.core.cache import cache
+from rest_framework.exceptions import AuthenticationFailed
+from .models import User
+
+
+class JWTAuthentication(authentication.BaseAuthentication):
+    jti = str(uuid.uuid4())
+
+    def authenticate(self, request, username=None, password=None):
+        authorization_header = request.headers.get("Authorization")
+        jwt_token = JWTAuthentication.get_token_form_header(authorization_header)
+
+        if jwt_token is None:
+            return None
+
+        try:
+            payload = jwt.decode(jwt_token, settings.SECRET_KEY, algorithms=["HS256"])
+        except jwt.exceptions.InvalidSignatureError:
+            raise AuthenticationFailed("Invalid signature")
+        except:
+            raise AuthenticationFailed("Invalid token")
+
+        user_id = payload.get("user_id")
+        jti = payload.get("jti")
+
+        if user_id is None:
+            raise AuthenticationFailed("User identifier not found in JWT")
+
+        if cache.get(jti) is None:
+            return None
+
+        user = User.objects.filter(id=user_id).first()
+
+        if user is None:
+            raise AuthenticationFailed("User not found")
+
+        return user, payload
