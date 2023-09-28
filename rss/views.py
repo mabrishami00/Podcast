9a05c6dc5ef421a5aa5a9143a184c6c05b10e2f1
mabrishami00 <mabrishami78@yahoo.com>
2023-09-29 00:15:49 +0330

    feat: add exceptions for main view

--- rss/views.py
+++ rss/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from django.db.models import Count
+
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
+
 from .serializers import RssSerializer, ChannelSerializer, PodcastSerializer
 from .parser import parser_for_rss_podcast
-from .models import Channel, Podcast
+from .models import Channel, Podcast, News
+from .tasks import parallel_parsing
 
-# Create your views here.
+from celery.result import AsyncResult
 
 
 class RssView(APIView):
@@ -14,16 +19,33 @@ class RssView(APIView):
     def get(self, request):
         url = request.query_params.get("url")
         channel = Channel.objects.get(url=url)
-        podcasts = Podcast.objects.filter(channel=channel)
+        results = (
+            Podcast.objects.filter(channel=channel)
+            if channel.channel_type == "p"
+            else News.objects.filter(channel=channel)
+        )
+
         sr_podcasts = PodcastSerializer(instance=podcasts, many=True)
         return Response(sr_podcasts.data)
 
     def post(self, request):
         sr_data = RssSerializer(data=request.POST)
         if sr_data.is_valid():
+            print(sr_data.data)
             vd = sr_data.validated_data
             url = vd["url"]
-            channel_type = vd["channel_type"]
-            if channel_type == "podcast":
-                parser_for_rss_podcast(url, channel_type)
-                return Response("ok")
+            result = parallel_parsing.delay(url)
+            task_id = result.task_id
+            return Response(task_id, status=status.HTTP_200_OK)
+        else:
+            url = request.POST.get("url")
+            if not url:
+                channels = Channel.objects.all()
+                (parallel_parsing.delay(channel.url) for channel in channels)
+                return Response(
+                    "All links are going to be updated!",
+                    status=status.HTTP_202_ACCEPTED,
+                )
+            return Response(
+                "This is not a valid url!", status=status.HTTP_400_BAD_REQUEST
+            )
