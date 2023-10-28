f7676c529dc0fa120ed6a68c76dd7a3339f0cc94
mabrishami00 <mabrishami78@yahoo.com>
2023-10-28 23:42:59 +0330

    feat: add last activity model

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from .managers import UserManager
 
+
 class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=100, null=True, blank=True)
     last_name = models.CharField(max_length=100, null=True, blank=True)
@@ -17,6 +18,8 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
 class Notification(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.TextField()
@@ -25,3 +28,11 @@ class Notification(models.Model):
     def __str__(self):
         return f"{self.user} | {self.message}"
 
+
+class UserLastActivity(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
+    activity = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} | {self.activity}"
