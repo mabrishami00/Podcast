f45ef75fc158521f6993f332960bffe2995c897e
mabrishami00 <mabrishami78@yahoo.com>
2023-10-28 23:42:45 +0330

    feat: add notification model

--- accounts/models.py
+++ accounts/models.py
@@ -17,4 +17,11 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
-    
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} | {self.message}"
+
