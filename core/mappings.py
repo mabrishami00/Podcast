b3dcc4a3569cbd2d907592dba3087c2a1eb91fea
mabrishami00 <mabrishami78@yahoo.com>
2023-10-28 23:48:54 +0330

    feat: add middleware mapping

---
+++ core/mappings.py
@@ -0,0 +1,17 @@
+mapping_middleware = {
+    "mappings": {
+        "properties": {
+            "user_id": {"type": "integer"},
+            "request_time": {"type": "date"},
+            "url_path": {"type": "text"},
+            "http_method": {"type": "keyword"},
+            "ip_address": {"type": "ip"},
+            "user_agent": {"type": "text"},
+            "status_code": {"type": "integer"},
+            "response_time": {"type": "date"},
+            "log_level": {"type": "text"},
+            "timestamp": {"type": "date"},
+        }
+    },
+}
+
