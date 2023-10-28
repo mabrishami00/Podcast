693dccebe3213e2c58f0ff03bc697d7c8744932f
mabrishami00 <mabrishami78@yahoo.com>
2023-10-29 00:24:21 +0330

    feat: add class methods for interactions models

--- interactions/models.py
+++ interactions/models.py
@@ -2,7 +2,7 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from accounts.models import User
-from rss.models import Channel
+from rss.models import Channel, Category
 
 
 # Create your models here.
@@ -15,7 +15,7 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.user
+        return f"{self.user}"
 
 
 class Like(models.Model):
@@ -24,16 +24,22 @@ class Like(models.Model):
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
 
-    @staticmethod
-    def is_liked(user, content_object):
-        return Like.objects.filter(user=user, content_object=content_object)
+    @classmethod
+    def is_liked(cls, user, content_type, object_id):
+        return cls.objects.filter(
+            user=user, content_type=content_type, object_id=object_id
+        ).exists()
 
-    @staticmethod
-    def count_like(content_object):
-        return Like.objects.filter(content_object=content_object).count()
+    @classmethod
+    def count_like(cls, content_type):
+        return cls.objects.filter(content_type=content_type).count()
+
+    @classmethod
+    def list_like(cls, user):
+        return cls.objects.filter(user=user)
 
     def __str__(self):
-        return self.user
+        return f"{self.user}"
 
 
 class Subscribe(models.Model):
@@ -42,13 +48,27 @@ class Subscribe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     is_subscribed = models.BooleanField(default=True)
 
-    @staticmethod
-    def is_subscribed(user, content_object):
-        return Subscribe.objects.filter(user=user, content_object=content_object)
+    @classmethod
+    def is_subscribed_method(cls, user, channel):
+        try:
+            result = cls.objects.get(user=user, channel=channel).is_subscribed
+        except Exception as e:
+            return False
+        else:
+            return result
 
-    @staticmethod
-    def count_subscribed(content_object):
-        return Subscribe.objects.filter(content_object=content_object).count()
+    @classmethod
+    def count_subscribe(cls, channel):
+        return cls.objects.filter(channel=channel, is_subscribed=True).count()
+
+    @classmethod
+    def list_subscribe(cls, user):
+        return cls.objects.filter(user=user)
+
+    @classmethod
+    def get_all_users_subscribe_channel(cls, channel):
+        return [subscribe.user for subscribe in cls.objects.filter(channel=channel)]
+
 
     def __str__(self):
         return f"{self.user} | {self.channel}"
@@ -61,13 +81,28 @@ class Bookmark(models.Model):
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
 
-    @staticmethod
-    def is_bookmarked(user, content_object):
-        return Bookmark.objects.filter(user=user, content_object=content_object)
+    @classmethod
+    def is_bookmarked(cls, user, content_type, object_id):
+        return cls.objects.filter(
+            user=user, content_type=content_type, object_id=object_id
+        ).exists()
 
-    @staticmethod
-    def count_bookmarked(content_object):
-        return Bookmark.objects.filter(content_object=content_object).count()
+    @classmethod
+    def count_bookmark(cls, content_type):
+        return cls.objects.filter(content_type=content_type).count()
+
+    @classmethod
+    def list_bookmark(cls, user):
+        return cls.objects.filter(user=user)
 
     def __str__(self):
-        return self.user
+        return f"{self.user}"
+
+
+class RecommendationPodcast(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    score = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.user} | {self.category} | {self.score}"
