369c141bb2e4b5e7b821ac2c8ca9213f61db9dd4
mabrishami00 <mabrishami78@yahoo.com>
2023-10-28 23:59:47 +0330

    feat: add comment serializer

--- interactions/serializers.py
+++ interactions/serializers.py
@@ -1,15 +1,6 @@
 from rest_framework import serializers
-from .models import Comment
+from .models import Comment, Like
+
 
 class CommentSerializer(serializers.Serializer):
-    class Meta:
-        model = Comment
-        fields = ["body"]
-
-    def save(self, user, content_object):
-        comment = super().save(*args, **kwargs)
-        comment.user = user
-        comment.content_object = content_object
-        comment.save()
-        return comment
-
+    body = serializers.CharField()
