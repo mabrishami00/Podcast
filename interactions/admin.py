a1ae45f0091e0a4bb8a6b4008e007fa97e92f0d0
mabrishami00 <mabrishami78@yahoo.com>
2023-10-29 00:24:41 +0330

    chore: add interactions models to admin panel

--- interactions/admin.py
+++ interactions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Like, Bookmark, Subscribe, Comment, RecommendationPodcast
 # Register your models here.
+
+admin.site.register(Like)
+admin.site.register(Bookmark)
+admin.site.register(Subscribe)
+admin.site.register(Comment)
+admin.site.register(RecommendationPodcast)
+
