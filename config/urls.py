5fd4b004e9a5fc777d18646d311e63548377b94d
mabrishami00 <mabrishami78@yahoo.com>
2023-10-29 00:04:18 +0330

    chore: add urls for config

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from accounts.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from accounts.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("rss.urls", namespace="rss")),
-    path("accounts/", include("accounts.urls", namespace="accounts")),
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "schema/swagger-ui/",
@@ -15,4 +17,11 @@ urlpatterns = [
     path(
         "schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"
     ),
+    path("rosetta/", include("rosetta.urls")),
+    path("admin/", admin.site.urls),
+    path("", include("rss.urls", namespace="rss")),
+    path("accounts/", include("accounts.urls", namespace="accounts")),
+    path("interactions/", include("interactions.urls", namespace="interactions")),
 ]
+
+# urlpatterns += i18n_patterns()
