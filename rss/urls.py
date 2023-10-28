f32a495812f0b6909cd36e979a3cc5ce533c1234
mabrishami00 <mabrishami78@yahoo.com>
2023-10-29 00:03:41 +0330

    chore: add urls for rss

--- rss/urls.py
+++ rss/urls.py
@@ -7,5 +7,13 @@ app_name = "rss"
 urlpatterns = [
     path("", views.RssView.as_view(), name="rss_reader"),
     path("check/", views.CheckWorkerJobView.as_view(), name="check_worker_job"),
-
+    path(
+        "get_all_channels/", views.GetAllChannelsView.as_view(), name="get_all_channels"
+    ),
+    path("get_podcasts/", views.GetPodcastsView.as_view(), name="get_podcasts"),
+    path(
+        "recommendation_podcast/",
+        views.RecommendationPodcastView.as_view(),
+        name="recommendation_podcast",
+    ),
 ]
